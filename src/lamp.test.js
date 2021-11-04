48695070a65280fd6cc0deefa1ad6b92a223929a
Michael Harrison <harrim91@hotmail.co.uk>
2021-11-04 00:34:01 +0000

    Update deps and comments

--- src/lamp.test.js
+++ src/lamp.test.js
@@ -2,10 +2,19 @@ const { Lamp } = require('./lamp');
 
 describe('Lamp', () => {
   describe('constructor', () => {
-    it('is turned off by default', () => {
-      // This jest.fn() is a DUMMY
-      // We never use it in the test, it's just a placeholder
+    it('has the given bulb', () => {
+      // This use of jest.fn() is a DUMMY
       // We could technically pass anything here, but jest.fn() indicates that it's just a fake thing
+      const bulb = jest.fn();
+      const lamp = new Lamp(bulb);
+
+      expect(lamp.bulb).toBe(bulb);
+    });
+
+    it('is turned off by default', () => {
+      // This use of jest.fn() is also a DUMMY
+      // We never even use it in the test, and don't even store it in a variable.  It's just a placeholder
+      // We could technically pass anything here, but jest.fn() again indicates that it's just a fake thing
       const lamp = new Lamp(jest.fn());
 
       expect(lamp.isOn).toBe(false);
@@ -18,12 +27,14 @@ describe('Lamp', () => {
       let lamp;
 
       beforeEach(() => {
-        // bulb here is a STUB
+        // For these tests, our bulb is a STUB
         // It's a simplified implementation that does only the job we need it to do, in a controlled way
-        // We need it to have an isWorking property, and an applyCurrent function
+        // Lamp.switchOn requires the bulb to have an isWorking property (boolean), and an applyCurrent property (a function)
         // N.B. we are using jest.fn() as a fake function
 
         bulb = {
+          // In these tests, we are checking how the lamp interacts with a WORKING bulb
+          // so we set isWorking to true
           isWorking: true,
           electrify: jest.fn(),
         };
@@ -41,6 +52,7 @@ describe('Lamp', () => {
       it('sends the correct voltage to the bulb', () => {
         lamp.switchOn();
 
+        // bulb.electrify is a jest mock - jest.fn() - so we can SPY on it!
         expect(bulb.electrify).toHaveBeenCalledWith(120);
       });
     });
@@ -55,6 +67,8 @@ describe('Lamp', () => {
         // We need it to have an isWorking property
 
         bulb = {
+          // In these tests, we are checking how the lamp interacts with a BROKEN bulb
+          // so we set isWorking to false
           isWorking: false,
           electrify: jest.fn(),
         };
@@ -96,7 +110,7 @@ describe('Lamp', () => {
       lamp.switchOn();
     });
 
-    it('turns the light on', () => {
+    it('turns the light off', () => {
       lamp.switchOff();
 
       expect(lamp.isOn).toBe(false);
